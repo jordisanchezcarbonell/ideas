9b74b2aacb05f119071ce51c834b785da692815a
Jordi Sánchez <jsanchez@eunoia.es>
2024-03-17 14:04:09 +0100

    Add web Web Analytics vercel

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { DM_Sans } from 'next/font/google';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
 import '../globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 import 'node_modules/flag-icons/css/flag-icons.min.css';
 import SelectorIdioma from '@/components/LocaleSwitcherSelect';
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </main>
+        <Analytics />
       </body>
     </html>
   );
