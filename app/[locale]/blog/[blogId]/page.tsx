5b07579e24415dab4b4c7a1b044e8a79c0fb1c00
Jordi Sánchez <jsanchez@eunoia.es>
2024-03-17 22:22:44 +0100

    Create component

--- app/[locale]/blog/[blogId]/page.tsx
+++ app/[locale]/blog/[blogId]/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 import { fetchAllSlugsE, fetchStrapiBySlug } from '../../action';
 import { locales } from '@/config';
+import { Suspense } from 'react';
+import DettaleBlog from '@/components/DetalleBlog';
 
 type BlogProps = {
   params: {
@@ -85,39 +87,9 @@ export async function generateStaticParams() {
 
 export default async function Blog({ params }: BlogProps) {
   const data = await fetchStrapiBySlug(params.blogId, params.locale);
-
-  const attributesDetalle = data[0].attributes;
-
   return (
-    <div className="p-4">
-      <Link
-        href="/"
-        type="button"
-        className="w-full flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700 mb-10"
-      >
-        <svg
-          className="w-5 h-5 rtl:rotate-180"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-          />
-        </svg>
-        Go back
-      </Link>
-
-      {attributesDetalle && attributesDetalle.Content && (
-        <div
-          className="whitespace-pre-line"
-          dangerouslySetInnerHTML={{ __html: attributesDetalle.Content }}
-        />
-      )}
-    </div>
+    <Suspense fallback={<h2>Loading...</h2>}>
+      <DettaleBlog promise={data} />
+    </Suspense>
   );
 }
